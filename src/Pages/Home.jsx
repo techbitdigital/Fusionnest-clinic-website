800b8becd94bbf4d3c2db9e61f4e6ebc8dd1f052
techbit <info@techbitdigital.com>
2026-05-11 14:24:02 +0100

    Integrate SEO hook into Home component

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,4 +1,6 @@
 import React, { useEffect } from "react";
+import useSEO from "../hooks/useSEO";
+import seoConfig from "../hooks/seoConfig";
 import Slider from "../Components/Slider";
 import Hero from "../Components/Hero";
 import Decade from "../Components/Decade";
@@ -10,9 +12,12 @@ import Testimony from "../Components/Testimony";
 import FAQs from "../Components/FAQs";
 
 export default function Home() {
+  useSEO(seoConfig.home);
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <div>
       <Hero />
