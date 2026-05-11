f3724b7576ad883fe1d0862a980e15036ae767a9
techbit <info@techbitdigital.com>
2026-05-11 14:21:21 +0100

    Update Blog.jsx

--- src/Pages/Blog.jsx
+++ src/Pages/Blog.jsx
@@ -1,9 +1,14 @@
 import React, { useEffect } from 'react'
+import useSEO from '../hooks/useSEO'
+import seoConfig from '../hooks/seoConfig'
 
 export default function Blog() {
+  useSEO(seoConfig.blog)
+
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
   return (
     <div>Blog</div>
   )
