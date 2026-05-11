f88f758caa3806546d91137786960e326e688ca6
techbit <info@techbitdigital.com>
2026-05-11 14:19:46 +0100

    Add SEO hook to AboutUs component
    Integrate SEO functionality into AboutUs component.

--- src/Pages/AboutUs.jsx
+++ src/Pages/AboutUs.jsx
@@ -1,15 +1,20 @@
 import React, { lazy, Suspense } from "react";
+import useSEO from "../hooks/useSEO";
+import seoConfig from "../hooks/seoConfig";
+
 const Hero = lazy(() => import("../Components/AboutFolder/AboutHero"));
 const WhoWeAre = lazy(() => import("../Components/AboutFolder/WhoWeAre"));
 const OurVision = lazy(() => import("../Components/AboutFolder/OurVision"));
 const AbtDoctors = lazy(() => import("../Components/AboutFolder/AbtDoctors"));
 
 export default function AboutUs() {
+  useSEO(seoConfig.about);
+
   return (
     <div>
       <Suspense
         fallback={
-          <div className="flex  items-center justify-center">
+          <div className="flex items-center justify-center">
             <img src="/assets/loading.gif" alt="Loading..." />
           </div>
         }
